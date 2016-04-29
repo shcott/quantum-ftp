8bb7dc84d29feb79833f9586ab552e65f24fbd9c
Scott <scott.huang.21@outlook.com>
2016-04-28 19:32:06 -0500

    Add login code

--- quantum.py
+++ quantum.py
@@ -1,4 +1,6 @@
 import sublime, sublime_plugin
+# from ftplib import FTP
+import ftplib
 
 class TestCommand(sublime_plugin.WindowCommand):
 	def run(self):
@@ -10,25 +12,20 @@ class ConnectCommand(sublime_plugin.WindowCommand):
 		defaultSettings = sublime.load_settings("Default.sublime-settings");
 		userSettings = sublime.load_settings("QuantumFTP.sublime-settings");
 
-		self.username = defaultSettings.get("quantum_username")
+		self.username = defaultSettings.get("username")
 		if self.username == "":
-			self.username = userSettings.get("quantum_username")
-		self.host = defaultSettings.get("quantum_host")
+			self.username = userSettings.get("username")
+		self.host = defaultSettings.get("host")
 		if self.host == "":
-			self.host = userSettings.get("quantum_host")
+			self.host = userSettings.get("host")
+		self.port = defaultSettings.get("port")
+		port = userSettings.get("port")
+		if port != 0:
+			self.port = port;
 
 		self.count = 0
 		self.password = ""
-		self.inputView = self.window.show_input_panel("Password:", "", self.on_done, self.on_change, 0)
-		
-	def on_done(self, input):
-		self.inputView = None
-		print("Password: {!s}".format(self.password))
-
-		# output = self.window.create_output_panel("quantum")
-		# self.window.run_command("show_panel", {"panel": "output.quantum", "toggle": True})
-		self.window.run_command("show_panel", {"panel": "console"})
-		print("Connecting to {!s}@{!s}".format(self.username, self.host))
+		self.inputView = self.window.show_input_panel("Password:", "", self.on_done, self.on_change, self.on_cancel)
 
 	def on_change(self, input):
 		self.count += 1
@@ -60,6 +57,32 @@ class ConnectCommand(sublime_plugin.WindowCommand):
 		else:
 			self.password += password[stars:]
 			self.inputView.run_command("hide_password", {"length": length})
+
+	def on_cancel(self):
+		self.inputView = None
+		self.password = ""
+
+	def on_done(self, input):
+		self.inputView = None
+		# print("Password: {!s}".format(self.password))
+
+		# output = self.window.create_output_panel("quantum")
+		# self.window.run_command("show_panel", {"panel": "output.quantum", "toggle": True})
+		self.window.run_command("show_panel", {"panel": "console"})
+		sublime.set_timeout_async(self.connect)
+
+	def connect(self):
+		print("Connecting to {!s}@{!s}".format(self.username, self.host))
+
+		ftp = ftplib.FTP()
+		ftp.connect(self.host, self.port)
+		if username == "":
+			if password == "":
+				ftp.login()
+		else:
+			ftp.login(self.username, self.password)
+
+		ftp.retrlines('LIST')
 
 
 class HidePasswordCommand(sublime_plugin.TextCommand):
